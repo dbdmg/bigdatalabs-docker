8ae65b6e9f07568558f3958a0fc381c837182b72
Luca Colomba <lccol338@gmail.com>
2024-11-05 17:10:11 +0100

    modified docker image name and tag

--- launchers/launch.sh
+++ launchers/launch.sh
@@ -21,4 +21,4 @@ fi
 echo -e "\e[1;31mLaunching the service... open this URL on the browser $URL\e[0m"
 echo -e "\e[1;31mYou can stop the container either with CTRL+C (CMD+C on mac) or with \"docker kill bdlabs\"\e[0m"
 echo -e "\e[1;31mYou can check if the container is still running with \"docker ps -a\"\e[0m"
-docker run -e BIGDATA_LISTEN_PORT=8001 -it --rm -p 8001:8001 -v $1:/vscode/workspace --name bdlabs bigdatalabs
+docker run -e BIGDATA_LISTEN_PORT=8001 -it --rm -p 8001:8001 -v $1:/vscode/workspace --name bdlabs lccol/bigdatalabs:v1.0.0
